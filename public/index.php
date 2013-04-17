10a54e1b3654fc3eb418c49295c027451d314126
Matthew Baggett <matthew@baggett.me>
2013-04-17 12:46:42 +0100

    Missing includes

--- public/index.php
+++ public/index.php
@@ -3,8 +3,13 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 ini_set('memory_limit', '2G');
 
+// Include Google PHP library
 require_once('../lib/google-api-php-client/src/Google_Client.php');
 require_once('../lib/google-api-php-client/src/contrib/Google_PlusService.php');
+require_once('../lib/google-api-php-client/src/contrib/Google_Oauth2Service.php');
+require_once('../lib/google-api-php-client/src/contrib/Google_LatitudeService.php');
+
+// Include t-core
 require_once("../lib/tcore/bootstrap.inc");
 
 \tcore\tcore::run();
