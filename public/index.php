361bc19dff4eefeb91f3e7f87a17eb39fe654d38
Matthew Baggett <matthew@baggett.me>
2013-04-17 12:43:48 +0100

    Adding Google PHP lib

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@ ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 ini_set('memory_limit', '2G');
 
+require_once('../lib/google-api-php-client/src/Google_Client.php');
+require_once('../lib/google-api-php-client/src/contrib/Google_PlusService.php');
 require_once("../lib/tcore/bootstrap.inc");
 
 \tcore\tcore::run();
