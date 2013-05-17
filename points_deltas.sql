f9f6b990f35f9c4fbfb56179b980ca084b30a633
Matthew Baggett <matthew@baggett.me>
2013-05-17 20:43:30 +0100

    New views

--- points_deltas.sql
+++ points_deltas.sql
@@ -1,29 +1,31 @@
 DROP VIEW IF EXISTS points_deltas ;
 CREATE VIEW points_deltas AS
-SELECT 
+SELECT
   p_a.uid as uid,
   p_a.pid as pid1,
-  p_a.latitude as lat1,
-  p_a.longitude as long1,
-  p_a.timestamp_ms as time1,
+  p_a.latitude as lat_1,
+  p_a.longitude as long_1,
+  p_a.timestamp_ms as time_ms_1,
+  p_a.timestamp_ms / 1000 as time_s_1,
   p_a.datetime as time_human_1,
-  p_b.pid as pid2,
-  p_b.latitude as lat2,
-  p_b.longitude as long2,
-  p_b.timestamp_ms as time2,
+  p_b.pid as pid_2,
+  p_b.latitude as lat_2,
+  p_b.longitude as long_2,
+  p_b.timestamp_ms as time_ms_2,
+  p_b.timestamp_ms / 1000 as time_s_2,
   p_b.datetime as time_human_2,
-  
+
   p_a.latitude - p_b.latitude as lat_delta,
   p_a.longitude - p_b.longitude as long_delta,
-  p_a.datetime - p_b.datetime as time_delta,
-  
-  ( 3959 * acos( cos( radians(p_b.latitude) ) 
-               * cos( radians( p_a.latitude ) ) 
-               * cos( radians( p_a.longitude ) - radians(p_b.longitude) ) 
-               + sin( radians( p_b.latitude	) ) 
+  p_a.datetime - p_b.datetime as time_delta_ms,
+  (p_a.datetime - p_b.datetime) / 1000 as time_delta_s,
+
+  ( 3959 * acos( cos( radians(p_b.latitude) )
+               * cos( radians( p_a.latitude ) )
+               * cos( radians( p_a.longitude ) - radians(p_b.longitude) )
+               + sin( radians( p_b.latitude	) )
                * sin( radians( p_a.latitude ) ) ) )
 	as distance_delta
-  
 
 FROM `points` p_a
 JOIN `points` p_b
--- points_view.sql
+++ points_view.sql
@@ -1,12 +1,21 @@
 DROP VIEW IF EXISTS points_view ;
 CREATE VIEW points_view AS
-SELECT 
-  p.uid,
-  p.pid,
-  p.latitude,
-  p.longitude,
-  p.timestamp_ms,
-  from_unixtime(p.timestamp_ms/1000) as time_human
+SELECT
+  pd.uid as uid,
+  pd.pid1 as pid,
 
-FROM `points` p
+  pd.time_ms_1 / 1 as time_ms,
+  pd.time_ms_1 / 1000 as time_s,
+  pd.time_human_1 as time_human,
+
+
+  pd.lat_delta as lat_delta,
+  pd.long_delta as long_delta,
+  pd.time_delta_ms as time_delta_ms,
+  pd.time_delta_s as time_delta_s,
+
+  pd.distance_delta as distance_delta
+
+FROM `points_deltas` pd
+ORDER BY time_s DESC
 
