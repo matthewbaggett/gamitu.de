0547324b4cd74290b6f27e15f460ecf5d57ebc50
Matthew Baggett <matthew@baggett.me>
2013-05-17 22:46:09 +0100

    Fixed Views

--- points_deltas.sql
+++ points_deltas.sql
@@ -1,32 +1,54 @@
 DROP VIEW IF EXISTS points_deltas ;
 CREATE VIEW points_deltas AS
 SELECT
-  p_a.uid as uid,
-  p_a.pid as pid1,
-  p_a.latitude as lat_1,
-  p_a.longitude as long_1,
-  p_a.timestamp_ms as time_ms_1,
-  p_a.timestamp_ms / 1000 as time_s_1,
-  p_a.datetime as time_human_1,
-  p_b.pid as pid_2,
-  p_b.latitude as lat_2,
-  p_b.longitude as long_2,
-  p_b.timestamp_ms as time_ms_2,
-  p_b.timestamp_ms / 1000 as time_s_2,
-  p_b.datetime as time_human_2,
+  current.uid as uid,
+  u.username as username,
+  current.pid as pid1,
+  current.latitude as lat_curr,
+  current.longitude as long_curr,
+  current.timestamp_ms as time_ms_curr,
+  current.timestamp_ms / 1000 as time_s_curr,
+  current.datetime as time_human_curr,
+  previous.pid as pid_prev,
+  previous.latitude as lat_prev,
+  previous.longitude as long_prev,
+  previous.timestamp_ms as time_ms_prev,
+  previous.timestamp_ms / 1000 as time_s_prev,
+  previous.datetime as time_human_prev,
 
-  p_a.latitude - p_b.latitude as lat_delta,
-  p_a.longitude - p_b.longitude as long_delta,
-  p_a.datetime - p_b.datetime as time_delta_ms,
-  (p_a.datetime - p_b.datetime) / 1000 as time_delta_s,
+  current.latitude - previous.latitude as lat_delta,
+  current.longitude - previous.longitude as long_delta,
+  current.datetime - previous.datetime as time_delta_ms,
+  (current.datetime - previous.datetime) / 1000 as time_delta_s,
 
-  ( 3959 * acos( cos( radians(p_b.latitude) )
-               * cos( radians( p_a.latitude ) )
-               * cos( radians( p_a.longitude ) - radians(p_b.longitude) )
-               + sin( radians( p_b.latitude	) )
-               * sin( radians( p_a.latitude ) ) ) )
-	as distance_delta
+  CASE
+    WHEN
+      current.latitude - previous.latitude != 0 OR current.longitude - previous.longitude != 0
+    THEN
+      ( 3959 * acos( cos( radians(previous.latitude) )
+               * cos( radians( current.latitude ) )
+               * cos( radians( current.longitude ) - radians(previous.longitude) )
+               + sin( radians( previous.latitude	) )
+               * sin( radians( current.latitude ) ) ) )
+    ELSE
+      0
+    END
+	as distance_delta,
 
-FROM `points` p_a
-JOIN `points` p_b
-  ON p_a.pid_preceding = p_b.pid AND p_a.uid = p_b.uid
+  CASE
+    WHEN
+      current.latitude - previous.latitude != 0 OR current.longitude - previous.longitude != 0
+    THEN
+      'Yes'
+    ELSE
+      'No'
+    END
+  as changed_state
+
+
+FROM `points` current
+JOIN `points` previous
+  ON  current.pid_preceding = previous.pid
+  AND current.uid = previous.uid
+JOIN `users` u
+  ON u.uid = current.uid
--- points_view.sql
+++ points_view.sql
@@ -2,11 +2,12 @@ DROP VIEW IF EXISTS points_view ;
 CREATE VIEW points_view AS
 SELECT
   pd.uid as uid,
+  pd.username as username,
   pd.pid1 as pid,
 
-  pd.time_ms_1 / 1 as time_ms,
-  pd.time_ms_1 / 1000 as time_s,
-  pd.time_human_1 as time_human,
+  pd.time_ms_curr / 1 as time_ms,
+  pd.time_ms_curr / 1000 as time_s,
+  pd.time_human_curr as time_human,
 
 
   pd.lat_delta as lat_delta,
@@ -14,7 +15,8 @@ SELECT
   pd.time_delta_ms as time_delta_ms,
   pd.time_delta_s as time_delta_s,
 
-  pd.distance_delta as distance_delta
+  pd.distance_delta as distance_delta,
+  pd.changed_state as changed_state
 
 FROM `points_deltas` pd
 ORDER BY time_s DESC
